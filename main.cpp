c2518e0e07f1047771cec3c5e312d46ba6057b66
Ricky Giese <richard.giese@icloud.com>
2023-10-19 20:05:40 -0500

    Fruit

--- main.cpp
+++ main.cpp
@@ -29,7 +29,11 @@ void Draw()
         {
             if (j == 0)
                 cout << "#";
-
+            if (i == y && j == x)
+                cout << "O";
+            else if (i == fruitY && j == fruitX)
+                cout << "F";
+            else
                 cout << " ";
             
             if (j == width - 1)
