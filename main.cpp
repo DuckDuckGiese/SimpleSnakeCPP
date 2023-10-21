a369bceab54db47be657f9e92eb0e21b668c7ac8
Ricky Giese <richard.giese@icloud.com>
2023-10-21 11:31:27 -0500

    wasd

--- main.cpp
+++ main.cpp
@@ -49,7 +49,24 @@ void Draw()
 }
 void Input()
 {
-
+    if (_kbhit())
+    {
+        switch (_getch())
+        {
+        case 'a':
+            dir = LEFT;
+            break;
+        case 'd':
+            dir = RIGHT;
+            break;
+        case 'w':
+            dir = UP;
+            break;
+        case 's':
+            dir = DOWN;
+            break;
+        }
+    }
 }
 void Logic()
 {
